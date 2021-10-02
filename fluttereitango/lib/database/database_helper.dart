f7e9c318f35b0ee216f6ae28b69b2ce3079f2228
shunsu <shunsuke8935@gmail.com>
2021-10-02 17:10:28 +0900

    20211002develop

---
+++ fluttereitango/lib/database/database_helper.dart
@@ -0,0 +1,77 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class Memo {
+  final int id;
+  final String text;
+
+  Memo({required this.id, required this.text});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'text': text,
+    };
+  }
+
+  static Future<Database> get database async {
+    // openDatabase() データベースに接続
+    final Future<Database> _database = openDatabase(
+      // getDatabasesPath() データベースファイルを保存するパス取得
+      join(await getDatabasesPath(), 'memo_database.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          // テーブルの作成
+          "CREATE TABLE memo(id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT)",
+        );
+      },
+      version: 1,
+    );
+    return _database;
+  }
+
+  //データの挿入
+  static Future<void> insertMemo(Memo memo) async {
+    final Database db = await database;
+    await db.insert(
+      'memo',
+      memo.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  //データ取得
+  static Future<List<Memo>> getMemos() async {
+    final Database db = await database;
+    final List<Map<String, dynamic>> maps = await db.query('memo');
+    return List.generate(maps.length, (i) {
+      return Memo(
+        id: maps[i]['id'],
+        text: maps[i]['text'],
+      );
+    });
+  }
+
+  //データのアップデート
+  static Future<void> updateMemo(Memo memo) async {
+    // Get a reference to the database.
+    final db = await database;
+    await db.update(
+      'memo',
+      memo.toMap(),
+      where: "id = ?",
+      whereArgs: [memo.id],
+      conflictAlgorithm: ConflictAlgorithm.fail,
+    );
+  }
+
+  //データの削除
+  static Future<void> deleteMemo(int id) async {
+    final db = await database;
+    await db.delete(
+      'memo',
+      where: "id = ?",
+      whereArgs: [id],
+    );
+  }
+}
--- fluttereitango/lib/pages/toppage/optimizenow.dart
+++ fluttereitango/lib/pages/toppage/optimizenow.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttereitango/database/database_helper.dart';
 
 import '../../main.dart';
 
@@ -15,7 +16,12 @@ class OptimizeNow extends StatelessWidget {
             borderRadius: BorderRadius.circular(24),
           ),
         ),
-        onPressed: () {},
+        onPressed: () async {
+          Memo _memo = Memo(text: 'テスト', id: 3);
+          final List<Memo> memos = await Memo.getMemos();
+          print(memos[0].id);
+          print(memos);
+        },
         child: Text('Optimize Now', style: TextStyle(color: Colors.white)),
       ),
     );
